2197b23d37d8f7d1a3d066fbce775368cd2d5b86
Marina Papapetrou <contact@marinapapap.com>
2023-10-22 17:24:47 +0100

    created 2 tests prior to development and created arraySum file

---
+++ array-sum/arraySum.test.js
@@ -0,0 +1,13 @@
+const arraySum = require("./arraySum");
+
+describe("sumArray", () => {
+  it("returns the sum of an array with length of 1", () => {
+    const array = [1];
+    expect(arraySum(array)).toBe(1);
+  });
+
+  it("returns the sum of an array with length of 2", () => {
+    const array = [1, 1];
+    expect(arraySum(array)).toBe(2);
+  });
+});
