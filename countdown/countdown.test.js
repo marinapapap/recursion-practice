a81693846d39a1b0293324b55c0338776afda0db
Marina Papapetrou <contact@marinapapap.com>
2023-10-24 22:59:27 +0100

    Passed test that passes a larger starting value into countdown

--- countdown/countdown.test.js
+++ countdown/countdown.test.js
@@ -10,4 +10,10 @@ describe("countdown", () => {
     const startingValue = 1;
     expect(countdown(startingValue)).toEqual([1, 0]);
   });
+
+  it("returns a countdown within an array from a given starting value to 0", () => {
+    const startingValue = 10;
+    const result = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    expect(countdown(startingValue)).toEqual(result);
+  });
 });
