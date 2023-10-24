3ccb631d4d3dc662c44b85f25e8c7ca1b25d5afc
Marina Papapetrou <contact@marinapapap.com>
2023-10-24 22:51:39 +0100

    Passed second test for countdown function that returns [1, 0] when given 1

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,5 +1,9 @@
 const countdown = (startingValue) => {
-  return [startingValue];
+  let countdown = [];
+  for (let i = startingValue; i >= 0; i--) {
+    countdown.push(i);
+  }
+  return countdown;
 };
 
 module.exports = countdown;
