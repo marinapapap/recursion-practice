23cef1afa2ab546972f7b767569d7cfb63eabce7
Marina Papapetrou <contact@marinapapap.com>
2023-10-28 15:37:44 +0100

    Created 2 tests for stringLength function - return 0 and 1

---
+++ string-length/stringLength.test.js
@@ -0,0 +1,13 @@
+const stringLength = require("./stringLength");
+
+describe("stringLength", () => {
+  it("returns 0 when given an empty string", () => {
+    const string = "";
+    expect(stringLength(string)).toEqual(0);
+  });
+
+  it("returns 1 when given an 'q", () => {
+    const string = "";
+    expect(stringLength(string)).toEqual(0);
+  });
+});
